0cf258266055647fa4c50b93d8f721aa75d76306
Viner <145243810+Hasper1337@users.noreply.github.com>
2025-12-23 17:43:05 +0500

    Update 5lab.py

--- 5lab.py
+++ 5lab.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
+from keras import Input
+from keras import Model
+from keras.layers import Concatenate
 import tensorflow as tf
 from sklearn.utils import shuffle
 import pandas as pd
@@ -14,7 +17,7 @@ def first_step():
 
   def sinusoid(x):
       return tf.sin(x)
-  
+
   x = np.arange(-20, 20, 0.1)
   y = np.sin(x) + np.sin(np.sqrt(2)*x)
   # Разделить на обучающую и тестовую выборки.
@@ -48,11 +51,11 @@ def second_step():
 
   def sinusoid(x):
       return tf.sin(x)
-  
+
   x = np.arange(-20, 20, 0.1)
   y = np.sin(x) + np.sin(np.sqrt(2)*x)
 
-  
+
   # Разделить на обучающую и тестовую выборки.
   X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=13)
 
@@ -81,13 +84,67 @@ def second_step():
   predictions = model.predict(np.column_stack((t_test, fi_test)))
   plt.figure(figsize=(10., 10.))
   plt.plot(x, y, c='blue', label='T', marker='X')
-  #plt.scatter(X_test, model.predict(np.column_stack((t_test, fi_test))), c='green', label='тест')
 
   plt.title("\nточность обучения %.2f" % (accuracy*100))
-  plt.scatter(X_test, predictions, label='Предсказания', color='red') 
+  plt.scatter(X_test, predictions, label='Предсказания', color='red')
+
+def third_step():
+
+  def sinusoid(x):
+    return tf.sin(x)
+
+  x = np.arange(-20, 20, 0.1)
+  y = np.sin(x) + np.sin(np.sqrt(2)*x)
+
+  X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=13)
+
+  model_T = Sequential([
+      Dense(32, activation=sinusoid, input_shape=(1,)),
+      Dense(1)
+  ])
+
+  model_T.compile(loss='mse', optimizer='rmsprop', metrics=['mae'])
+  model_T.fit(X_train, X_train // (2 * np.pi), epochs=150, batch_size=10)
+
+  model_fi = Sequential([
+    Dense(64, activation='relu', input_shape=(1,)),
+    Dense(32, activation='relu'),
+    Dense(1)
+  ])
+
+  model_fi.compile(loss='mse', optimizer='rmsprop', metrics=['mae'])
+  model_fi.fit(X_train, X_train % (2 * np.pi), epochs=150, batch_size=10)
+
+  input_T = Input(shape=(1,))
+  input_fi = Input(shape=(1,))
+  concatenated = Concatenate(axis=-1)([model_T(input_T), model_fi(input_fi)])
+  output = Dense(16, activation=sinusoid)(concatenated)
+  output = Dense(1)(output)
+
+  model_combined = Model(inputs=[input_T, input_fi], outputs=output)
+
+  model_combined.compile(loss='mse', optimizer='rmsprop', metrics=['mae'])
+  model_combined.fit([X_train, X_train], y_train, epochs=150, batch_size=10)
+
+  _, accuracy = model_combined.evaluate([X_train, X_train], y_train)
+  print("Точность на обучающей выборке:", accuracy * 100)
+
+  _, accuracy2 = model_combined.evaluate([X_test, X_test], y_test)
+  print("Точность на тестовой выборке:", accuracy2 * 100)
+
+  y_pred = model_combined.predict([X_test, X_test])
+
+  plt.plot(x, y, label='f(x)')
+  plt.title("\nточность обучения %.2f" % (accuracy*100))
+  plt.scatter(X_test, y_pred, label='Предсказания', color='red', alpha=1)
+  plt.xlabel('x')
+  plt.ylabel('y')
+  plt.legend()
+  plt.show()
 
 
 
 if __name__ == '__main__':
   #first_step()
-  second_step()
+  #second_step()
+  third_step()
